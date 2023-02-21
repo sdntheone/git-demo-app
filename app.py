ee08879ed85ce67f879213c1d6600a16fd150e04
sdntheone <sdntheone@gmail.com>
2023-02-21 21:42:53 +0530

    change the subheader heading

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ with col2:
     this is website created for the youngesters of india to build a carrrer in the field of data science.
     Hi, my name is Nitish. I am an educational content creator on YouTube with 50K+ Subscribers in the domain of Data Science. I have been in the tech industry for the past 10 years and taught more than 50,000 students offline. I am passionate about data and take pride in creating content that simplifies complex topics.
     """)
-st.header("course offered")
+st.header("course offered by campusX and SDN industry")
 st.subheader('data science and machine learning')
 st.subheader('data engineer')
 st.subheader('deep learning')
