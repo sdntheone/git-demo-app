9713ae0c8859989a563189c91babc668edfae3db
sdntheone <sdntheone@gmail.com>
2023-02-21 13:22:21 +0530

    intial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+st.title('Campusx')
+
+col1,col2=st.columns(2)
+
+with col1:
+    st.image('sdn.jpg')
+with col2:
+    st.write("""
+    this is website created for the youngesters of india to build a carrrer in the field of data science.
+    Hi, my name is Nitish. I am an educational content creator on YouTube with 50K+ Subscribers in the domain of Data Science. I have been in the tech industry for the past 10 years and taught more than 50,000 students offline. I am passionate about data and take pride in creating content that simplifies complex topics.
+    """)
